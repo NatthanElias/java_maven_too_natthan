e93902b13cea672f105275d1aec3d14c0ed5d559
Natthan <natthandosantos@gmail.com>
2023-03-19 17:32:17 -0300

    Exerc01

--- src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Exercicio1.java
+++ src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Exercicio1.java
@@ -1,44 +1,65 @@
 package br.edu.ifsul.bcc.too.topico3.exercicios;
 
 /**
- *
  * @author Prof. Telmo Júnior
  */
 
 
 import java.util.Scanner;
 
-public class Exercicio1 {        
-    
+public class Exercicio1 {
+
     public static void main(String[] args) {
-        
+
         int[][] rgDados1 = new int[3][3];
-    
+
         int[][] rgDados2 = new int[3][3];
-        
+
         int[][] rgDados3 = new int[3][3];
-    
+
         /*
             1) Com base no método hasNext da classe Scanner obtenha informações do console e inicializar as matrizes rgDados 1 e rgDados2.
                Em seguida finalize a rotina para somar os dados das matrizes (rgDados1 e rgDados2) e armazene em rgDados3. Por fim imprima no console os dados gerados (rgDados3).
-        
+
                Documentação: https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
         */
-        
-        Scanner sc = new Scanner(System.in);
-        
-        for (int linha=0; linha < rgDados1.length; linha++){
-            
-            for(int coluna=0; coluna < rgDados1[linha].length; coluna++){
-                
-                System.out.println("Digite um valor: ");
-                if(sc.hasNext()){
-                    // sc.next(); ...
-                }                
-            }                        
-        }       
 
-        
+        Scanner sc = new Scanner(System.in);
+
+        for (int linha = 0; linha < rgDados1.length; linha++) {
+
+            for (int coluna = 0; coluna < rgDados1[linha].length; coluna++) {
+
+                System.out.println("Digite um valor para a primeira matriz: ");
+                if (sc.hasNext()) {
+                    rgDados1[linha][coluna] = sc.nextInt();
+                }
+            }
+        }
+
+        for (int linha = 0; linha < rgDados2.length; linha++) {
+
+            for (int coluna = 0; coluna < rgDados2[linha].length; coluna++) {
+
+                System.out.println("Digite um valor para a segunda matriz: ");
+                if (sc.hasNext()) {
+                    rgDados2[linha][coluna] = sc.nextInt();
+                }
+            }
+        }
+
+        System.out.println("Matriz resultante da soma das duas informadas: ");
+        for (int linha = 0; linha < rgDados2.length; linha++) {
+
+            for (int coluna = 0; coluna < rgDados2[linha].length; coluna++) {
+                if (sc.hasNext()) {
+                    rgDados3[linha][coluna] = rgDados1[linha][coluna] + rgDados2[linha][coluna];
+                    System.out.print(rgDados3[linha][coluna] + "\t");
+                }
+            }
+            System.out.println();
+        }
+
     }
-    
+
 }
